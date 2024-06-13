aeafe8e3ea4b16ca76639303751c057bf621a691
khadija <khadijajelle3@gmail.com>
2024-06-13 16:36:55 +0300

    complete javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+  function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+  }
+
+  function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    };
+  }
+
+  // Create a highlighter function using '*'
+  const highlighter = wrapAdjective("*");
+
+  // Test the function
+  console.log(highlighter("awesome")); // Output: You are *awesome*!
+  console.log(highlighter("amazing")); // Output: You are *amazing*!
